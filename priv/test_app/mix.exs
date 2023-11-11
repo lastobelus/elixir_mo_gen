a0608504eeac925685f6301ddd0c40808d087637
Michael Johnston <lastobelus@mac.com>
2023-11-11 09:07:53 -0800

    test: update test_app elixir & use prune_code_paths
    without prune_code_paths, test_app fails to compile
    when calling  in the
    context of the test_app in a test

--- priv/test_app/mix.exs
+++ priv/test_app/mix.exs
@@ -5,9 +5,14 @@ defmodule TestApp.MixProject do
     [
       app: :test_app,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # the following option is necessary for tests to switch successfully
+      # to the test app environment inside a `Mix.Project.in_project` block
+      # and run a generator. Otherwise, the compile that mix does first will
+      # fail, even though the app is already built
+      prune_code_paths: false
     ]
   end
 
@@ -20,7 +25,7 @@ defmodule TestApp.MixProject do
 
   defp deps do
     [
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"}
     ]
   end
