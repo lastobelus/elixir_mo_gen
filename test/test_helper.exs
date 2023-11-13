4798736af1527f59a208affa68b77e3764deb229
Michael Johnston <lastobelus@mac.com>
2023-11-13 11:38:58 -0800

    tests: build test_app at start of test run

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,10 @@
 ExUnit.start()
+
+File.cd!("priv/test_app", fn ->
+  IO.puts("compiling test_app")
+  {output, result} = System.cmd("mix", [], stderr_to_stdout: true, env: [{"MIX_ENV", "test"}])
+
+  if result != 0 do
+    Mix.raise("test_app compile failed:\n#{output}")
+  end
+end)
