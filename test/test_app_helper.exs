ab458286f3d65daf986b101af81dc3b9733b8ca5
Michael Johnston <lastobelus@mac.com>
2023-11-13 11:37:58 -0800

    tests: provide in_test_app for testing generators
    in the context of an actual app

--- test/test_app_helper.exs
+++ test/test_app_helper.exs
@@ -1,13 +1,53 @@
 defmodule TestAppHelper do
   @test_app_path "priv/test_app"
 
-  def in_test_app(test, func) do
-    migration_path = Path.expand()
 
-    try do
-      File.cd!(path, function)
-    after
-      File.rm_rf!(migration_path)
+  def in_test_app(tmp_dir, func) do
+    # dbg(tmp_dir)
+
+    test_app_path = Path.expand(@test_app_path)
+
+    File.cd!(tmp_dir, fn ->
+      File.cp_r!(
+        test_app_path,
+        "test_app"
+      )
+
+      File.cd!("test_app", fn ->
+        ignoring_module_conflicts( fn ->
+          Mix.Project.in_project(:test_app, ".", fn _module ->
+            func.()
+          end)
+        end)
+      end)
+    end)
+
+  end
+
+  def ignoring_module_conflicts(func) do
+    # prevent warnings about redefining modules from clogging test output
+    orig_ignore_module_conflict = Code.get_compiler_option(:ignore_module_conflict)
+    Code.put_compiler_option(:ignore_module_conflict, true)
+
+    func.()
+
+    Code.put_compiler_option(:ignore_module_conflict, orig_ignore_module_conflict)
+  end
+
+  def run_mix_test(test, opts \\ []) do
+    System.cmd("mix", ~w(clean))
+
+    {output, _exit_status} = System.cmd("mix", ~w(test), stderr_to_stdout: true)
+
+    [deps_build_output | test_output] = String.split(output, "==> #{@test_app_name}\nCompiling")
+
+    cond do
+      length(test_output) < 1 ->
+        {:error, deps_build_output}
+
+      true ->
+        {:ok, Enum.join(test_output)}
     end
   end
+
 end
